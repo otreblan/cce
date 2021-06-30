89a25a17524eadb1f32a94af2a9d65d4658a2b27
otreblan <otreblain@gmail.com>
2021-06-30 08:40:52 -0500

    call.c

--- src/ast/call.c
+++ src/ast/call.c
@@ -13,3 +13,49 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with cce.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <stdlib.h>
+
+#include "args.h"
+#include "call.h"
+
+static ast_call* ast_call_alloc();
+
+static ast_call* ast_call_alloc()
+{
+	ast_call* call = malloc(sizeof(ast_call));
+
+	if(call)
+	{
+		*call = (ast_call)
+		{
+			.ID   = NULL,
+			.args = NULL
+		};
+	}
+
+	return call;
+}
+
+ast_call* ast_call1(char* ID, ast_args* args)
+{
+	ast_call* call = ast_call_alloc();
+
+	if(call)
+	{
+		call->ID   = ID;
+		call->args = args;
+	}
+
+	return call;
+}
+
+void ast_call_free(ast_call* call)
+{
+	if(call)
+	{
+		free(call->ID);
+		ast_args_free(call->args);
+	}
+	free(call);
+}
