b935c6c189cc4b8e6991cc3bea7225821c95884e
otreblan <otreblain@gmail.com>
2021-06-30 09:38:36 -0500

    var.c parte 1

--- src/ast/var.c
+++ src/ast/var.c
@@ -13,3 +13,56 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with cce.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <stdlib.h>
+
+#include "expresion.h"
+#include "var.h"
+
+static ast_var* ast_var_alloc();
+
+static ast_var* ast_var_alloc()
+{
+	ast_var* var = malloc(sizeof(ast_var));
+
+	if(var)
+	{
+		*var = (ast_var)
+		{
+			.ID        = NULL,
+			.expresion = NULL
+		};
+	}
+
+	return var;
+}
+
+ast_var* ast_var1(char* ID)
+{
+	ast_var* var = ast_var_alloc();
+
+	if(var)
+		var->ID = ID;
+
+	return var;
+}
+
+ast_var* ast_var2(char* ID, ast_expresion* expresion)
+{
+	ast_var* var = ast_var1(ID);
+
+	if(var)
+		var->expresion = expresion;
+
+	return var;
+}
+
+void ast_var_free(ast_var* var)
+{
+	if(var)
+	{
+		free(var->ID);
+		//ast_expresion_free(var->expresion);
+	}
+	free(var);
+}
