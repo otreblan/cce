ec156b4991b12f106b50743f459f4ef772577da2
otreblan <otreblain@gmail.com>
2021-06-30 10:44:05 -0500

    declaracion_local.c

--- src/ast/declaracion_local.c
+++ src/ast/declaracion_local.c
@@ -13,3 +13,57 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with cce.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <stdlib.h>
+
+#include "declaracion_local.h"
+#include "var_declaracion.h"
+
+static ast_declaracion_local* ast_declaracion_local_alloc();
+
+static ast_declaracion_local* ast_declaracion_local_alloc()
+{
+	ast_declaracion_local* declaracion_local = malloc(sizeof(ast_declaracion_local));
+
+	if(declaracion_local)
+	{
+		*declaracion_local = (ast_declaracion_local)
+		{
+			.next            = NULL,
+			.var_declaracion = NULL
+		};
+	}
+
+	return declaracion_local;
+}
+
+ast_declaracion_local* ast_declaracion_local1(
+	ast_declaracion_local* next,
+	ast_var_declaracion*   var_declaracion
+)
+{
+	ast_declaracion_local* declaracion_local = ast_declaracion_local_alloc();
+
+	if(declaracion_local)
+	{
+		declaracion_local->next            = next;
+		declaracion_local->var_declaracion = var_declaracion;
+	}
+
+	return declaracion_local;
+}
+
+ast_declaracion_local* ast_declaracion_local2()
+{
+	return ast_declaracion_local_alloc();
+}
+
+void ast_declaracion_local_free(ast_declaracion_local* declaracion_local)
+{
+	if(declaracion_local)
+	{
+		ast_declaracion_local_free(declaracion_local->next);
+		ast_var_declaracion_free(declaracion_local->var_declaracion);
+	}
+	free(declaracion_local);
+}
