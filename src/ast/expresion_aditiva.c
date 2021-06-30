3805300bf1011c6aa2cec74cdf2ea8f105c8aaa0
otreblan <otreblain@gmail.com>
2021-06-30 09:15:41 -0500

    expresion_aditiva.c

--- src/ast/expresion_aditiva.c
+++ src/ast/expresion_aditiva.c
@@ -13,3 +13,66 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with cce.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <stdlib.h>
+
+#include "addop.h"
+#include "expresion_aditiva.h"
+#include "term.h"
+
+static ast_expresion_aditiva* ast_expresion_aditiva_alloc();
+
+static ast_expresion_aditiva* ast_expresion_aditiva_alloc()
+{
+	ast_expresion_aditiva* expresion_aditiva = malloc(sizeof(expresion_aditiva));
+
+	if(expresion_aditiva)
+	{
+		*expresion_aditiva = (ast_expresion_aditiva)
+		{
+			.expresion_aditiva = NULL,
+			.addop             = NULL,
+			.term              = NULL
+		};
+	}
+
+	return expresion_aditiva;
+}
+
+ast_expresion_aditiva* ast_expresion_aditiva1(
+	ast_expresion_aditiva* expresion_aditiva,
+	ast_addop*             addop,
+	ast_term*              term
+)
+{
+	ast_expresion_aditiva* _expresion_aditiva = ast_expresion_aditiva2(term);
+
+	if(_expresion_aditiva)
+	{
+		_expresion_aditiva->expresion_aditiva = expresion_aditiva;
+		_expresion_aditiva->addop             = addop;
+	}
+
+	return _expresion_aditiva;
+}
+
+ast_expresion_aditiva* ast_expresion_aditiva2(ast_term* term)
+{
+	ast_expresion_aditiva* expresion_aditiva = ast_expresion_aditiva_alloc();
+
+	if(expresion_aditiva)
+		expresion_aditiva->term = term;
+
+	return expresion_aditiva;
+}
+
+void ast_expresion_aditiva_free(ast_expresion_aditiva* expresion_aditiva)
+{
+	if(expresion_aditiva)
+	{
+		ast_expresion_aditiva_free(expresion_aditiva->expresion_aditiva);
+		ast_addop_free(expresion_aditiva->addop);
+		ast_term_free(expresion_aditiva->term);
+	}
+	free(expresion_aditiva);
+}
