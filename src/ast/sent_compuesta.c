68de7095df0b22c196ed28be44cea2a37dd04975
otreblan <otreblain@gmail.com>
2021-06-30 10:51:46 -0500

    sent_compuesta.c

--- src/ast/sent_compuesta.c
+++ src/ast/sent_compuesta.c
@@ -13,3 +13,53 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with cce.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <stdlib.h>
+
+#include "declaracion_local.h"
+#include "lista_sentencias.h"
+#include "sent_compuesta.h"
+
+static ast_sent_compuesta* ast_sent_compuesta_alloc();
+
+static ast_sent_compuesta* ast_sent_compuesta_alloc()
+{
+	ast_sent_compuesta* sent_compuesta = malloc(sizeof(ast_sent_compuesta));
+
+	if(sent_compuesta)
+	{
+		*sent_compuesta = (ast_sent_compuesta)
+		{
+			.declaracion_local = NULL,
+			.lista_sentencias  = NULL
+		};
+	}
+
+	return sent_compuesta;
+}
+
+ast_sent_compuesta* ast_sent_compuesta1(
+	ast_declaracion_local* declaracion_local,
+	ast_lista_sentencias*  lista_sentencias
+)
+{
+	ast_sent_compuesta* sent_compuesta = ast_sent_compuesta_alloc();
+
+	if(sent_compuesta)
+	{
+		sent_compuesta->declaracion_local = declaracion_local;
+		sent_compuesta->lista_sentencias  = lista_sentencias;
+	}
+
+	return sent_compuesta;
+}
+
+void ast_sent_compuesta_free(ast_sent_compuesta* sent_compuesta)
+{
+	if(sent_compuesta)
+	{
+		ast_declaracion_local_free(sent_compuesta->declaracion_local);
+		ast_lista_sentencias_free(sent_compuesta->lista_sentencias);
+	}
+	free(sent_compuesta);
+}
